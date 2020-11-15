e8d9a3d32ab88a885f3e057757b6481371e917f5
kamil0x13 <pietrzakkamil013@gmail.com>
2020-11-15 13:28:54 +0100

    dataBase url

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 const PORT = 3001
-const MONGODB_URL = 'mongodb://127.0.0.1:27017/Serwis-test'
-//'mongodb+srv://admin:Haslo@cluster0-af3id.mongodb.net/Serwis_Sprzetu?retryWrites=true&w=majority'
+const MONGODB_URL = 'mongodb+srv://admin:Haslo@cluster0-af3id.mongodb.net/Serwis_Sprzetu?retryWrites=true&w=majority'
+//'mongodb://127.0.0.1:27017/Serwis-test'
 const JWT_SECRET = 'SecretFroGit'
 
 module.exports = {
